4c7e8e6e7088547b0a94029a9a07ade65fa8db32
Juan Q <juanqi3489@gmail.com>
2025-06-08 14:43:26 -0300

    Se agrego un boton para encender y apagar la cámara | Se agrego un panel de control para cambiar ciertas cosas del detector de rostros

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const video = document.getElementById('video');
+        const video = document.getElementById('video');
         const canvas = document.getElementById('canvas');
         const ctx = canvas.getContext('2d');
+        const controlCamara = document.getElementById('controlCamara');
+        const overlay = document.getElementById('overlay');
+        const videoContainer = document.getElementById('video-container')
+
+        let stream = null;
+        let model = null;
+        let camaraPrendida = true;
+        let animationFrameId = null;
 
         async function setupCamera() {
-        const stream = await navigator.mediaDevices.getUserMedia({
-            video: true,
-        });
+        stream = await navigator.mediaDevices.getUserMedia({video: true,});
         video.srcObject = stream;
 
         return new Promise((resolve) => {
@@ -16,22 +22,26 @@ const video = document.getElementById('video');
         }
 
         async function run() {
-        // ✅ Registrar el backend antes de usar modelos
         await tf.setBackend('webgl');
         await tf.ready();
 
         await setupCamera();
         video.play();
 
-        const model = await faceDetection.createDetector(
+        model = await faceDetection.createDetector(
             faceDetection.SupportedModels.MediaPipeFaceDetector,
-            {
-            runtime: 'tfjs',
-            }
+            {runtime: 'tfjs'}
         );
+        detectFaces();
+    }
 
         async function detectFaces() {
+            if (!camaraPrendida || !model) return;
+
             const faces = await model.estimateFaces(video);
+
+            if(!camaraPrendida) return;
+            
             ctx.clearRect(0, 0, canvas.width, canvas.height);
 
             if (Array.isArray(faces)){
@@ -50,11 +60,35 @@ const video = document.getElementById('video');
                     }
                 }
             }
-
-            requestAnimationFrame(detectFaces);
+        
+            if (camaraPrendida){
+                animationFrameId = requestAnimationFrame(detectFaces);
+            }
         }
 
-        detectFaces();
-        }
+        controlCamara.addEventListener('click', () =>{
+            if(camaraPrendida){
+                const tracks = stream.getTracks();
+                tracks.forEach(track => track.stop());
+                video.srcObject = null;
+                camaraPrendida = false;
+                cancelAnimationFrame(animationFrameId);
+                ctx.clearRect(0,0, canvas.width, canvas.height);
+                overlay.style.visibility = 'visible';
+                controlCamara.innerHTML = `<img src="assets/camera-video-off-fill.svg" alt="">`;
+                controlCamara.style.backgroundColor = 'rgb(255, 17, 17)';
+                videoContainer.style.borderColor = 'rgb(255, 17, 17)';
+            } else{
+                setupCamera().then(() =>{
+                    video.play();
+                    camaraPrendida = true;
+                    overlay.style.visibility = 'hidden';
+                    controlCamara.style.backgroundColor = 'white';
+                    videoContainer.style.borderColor = 'rgb(56, 52, 183)';
+                    controlCamara.innerHTML = `<img src="assets/camera-video-fill.svg" alt="">`;
+                    detectFaces();
+                });
+            }
+        });
 
         run();
